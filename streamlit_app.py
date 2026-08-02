7f78183e12bc33cfc962e864cf9bbc42a376b81f
Thenkuzhali-M <107546959+Thenkuzhali-M@users.noreply.github.com>
2026-08-02 19:21:14 +0530

    Fix nutrition header and update API request
    Updated the nutrition information header and modified the API request to use the search value.

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,9 +31,9 @@ if ingredients_list:
       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_choosen, 'SEARCH_ON'].iloc[0]
       st.write('The search value for ', fruits_choosen,' is ', search_on, '.')
       
-      st.subheader(fruits_choosen + 'Nutrition_information')
+      st.subheader(fruits_choosen + ' Nutrition_information')
       
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruits_choosen)  
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
       sf_df =  st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       
    

08bc394e00de0c992b95932a3026a0e812e786a5
Thenkuzhali-M <107546959+Thenkuzhali-M@users.noreply.github.com>
2026-08-02 19:25:41 +0530

    Fix API request URL construction for fruit data

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
       
       st.subheader(fruits_choosen + ' Nutrition_information')
       
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)  
       sf_df =  st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
       
    
